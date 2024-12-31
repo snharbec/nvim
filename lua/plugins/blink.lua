6de3b08477ac1ebd4c1001a2d505cb41fbe720b2
Stefan Harbeck <stefan@harbeck.name>
2024-12-31 10:51:30 +0100

    Completion
    - CR now completes
    - Tab cycles throught alternatives

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -15,15 +15,27 @@ return {
         ["<C-l>"] = { "select_and_accept" },
         ["<C-j>"] = { "select_next" },
         ["<C-k>"] = { "select_prev" },
+        ["<CR>"] = { "accept", "fallback" },
         ["<Tab>"] = {
           function(cmp)
             if cmp.snippet_active() then
               return cmp.accept()
             else
-              return cmp.select_and_accept()
+              return cmp.select_next()
             end
           end,
           "snippet_forward",
+          "fallback",
+        },
+        ["<S-Tab>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_prev()
+            end
+          end,
+          "snippet_backward",
           "fallback",
         },
       },
