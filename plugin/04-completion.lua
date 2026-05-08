dd6c18bb9b49d488c58e0066f4a773327f83a963
Stefan Harbeck <stefan.harbeck@saabgroup.com>
2026-05-08 11:24:45 +0200

    Remove CR to accept a completion

--- plugin/04-completion.lua
+++ plugin/04-completion.lua
@@ -21,7 +21,7 @@ cmp.setup({
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    -- ["<CR>"] = cmp.mapping.confirm({ select = true }),
     ["<Tab>"] = cmp.mapping.confirm({ select = true }),
     ["<Down>"] = cmp.mapping.select_next_item(),
     ["<Up>"] = cmp.mapping.select_prev_item(),
